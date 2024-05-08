b867cc929514a24dd8e85b1151dd6f2678cb11e1
DanielHQ <88734510+DanielHQ@users.noreply.github.com>
2024-05-08 00:56:15 -0300

    Add files via upload

--- js/categorias.js
+++ js/categorias.js
@@ -22,3 +22,12 @@ categorias.forEach((categoria) => {
 		});
 	});
 });
+
+const list = document.querySelectorAll('.list');
+function activeLink(){
+	list.forEach((item) => 
+item.classList.remove('active'));
+this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click', activeLink));
